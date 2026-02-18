bc5bd0d33cb27eb606e42a86fd64451c30c9277d
Zhitkov Nikita <zhitkov.ns@gmail.com>
2026-02-18 14:05:30 +0300

    final ver

--- lab1.py
+++ lab1.py
@@ -3,10 +3,9 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm, cauchy, laplace, uniform, poisson
 
 np.random.seed(42)
-# Параметры
+
 n_values = [10, 100, 1000]
 
-# Описание распределений
 distributions = {
     'normal': {
         'generator': np.random.normal,
@@ -15,7 +14,7 @@ distributions = {
         'pdf_args': (0, 1),
         'x_range': (-4, 4),
         'discrete': False,
-        'title': 'Нормальное N(0,1)'
+        'title': r'Нормальное $N(0,1)$'
     },
     'cauchy': {
         'generator': np.random.standard_cauchy,
@@ -24,7 +23,7 @@ distributions = {
         'pdf_args': (0, 1),
         'x_range': (-20, 20),
         'discrete': False,
-        'title': 'Коши C(0,1)'
+        'title': r'Коши $C(0,1)$'
     },
     'laplace': {
         'generator': np.random.laplace,
@@ -33,7 +32,7 @@ distributions = {
         'pdf_args': (0, 1/np.sqrt(2)),
         'x_range': (-6, 6),
         'discrete': False,
-        'title': 'Лапласа L(0, 1/√2)'
+        'title': r'Лапласа $L(0,1/\sqrt{2})$'
     },
     'poisson': {
         'generator': np.random.poisson,
@@ -42,7 +41,7 @@ distributions = {
         'pmf_args': (10,),
         'x_range': (0, 20),
         'discrete': True,
-        'title': 'Пуассона P(10)'
+        'title': r'Пуассона $P(10)$'
     },
     'uniform': {
         'generator': np.random.uniform,
@@ -51,7 +50,7 @@ distributions = {
         'pdf_args': (-np.sqrt(3), 2*np.sqrt(3)),
         'x_range': (-2, 2),
         'discrete': False,
-        'title': 'Равномерное U(−√3, √3)'
+        'title': r'Равномерное $U(-\sqrt{3},\sqrt{3})$'
     }
 }
 
@@ -61,30 +60,61 @@ for name, dist in distributions.items():
 
     for i, n in enumerate(n_values):
         ax = axes[i]
-
-        # Генерация выборки
         data = dist['generator'](*dist['gen_args'], n)
 
         if dist['discrete']:
-            # Пуассон – дискретное распределение
-            bins = np.arange(-0.5, 20.5, 1)  # фиксированные бины для целых значений
-            ax.hist(data, bins=bins, density=True, alpha=0.7, edgecolor='black', label='Гистограмма') # density - это нормировка, чтобы площадь под графиком была равна 1
-            # Теоретические вероятности
+            # Пуассон
+            if n <= 100:
+                # Для малых выборок используем автоматический подбор бинов
+                bins = 'auto'
+                # Ограничиваем диапазон, чтобы избежать лишних хвостов
+                ax.hist(data, bins=bins, range=(0, 20), density=True, alpha=0.7,
+                        edgecolor='black', label='Гистограмма')
+            else:
+                # Для n=1000 используем точные бины шириной 1
+                bins = np.arange(-0.5, 20.5, 1)
+                ax.hist(data, bins=bins, density=True, alpha=0.7, edgecolor='black',
+                        label='Гистограмма')
+            # Теоретические плотности
             x_vals = np.arange(0, 21)
             pmf_vals = dist['pmf_func'](x_vals, *dist['pmf_args'])
             ax.plot(x_vals, pmf_vals, 'ro-', markersize=4, label='Теоретическая вероятность')
+            ax.set_xlim(0, 20)
             ax.set_xlabel('k')
         else:
             # Непрерывные распределения
-            ax.hist(data, bins='auto', density=True, alpha=0.7, edgecolor='black', label='Гистограмма') # density - это нормировка, чтобы площадь под графиком была равна 1
-            x_vals = np.linspace(dist['x_range'][0], dist['x_range'][1], 200)
-            pdf_vals = dist['pdf_func'](x_vals, *dist['pdf_args'])
-            ax.plot(x_vals, pdf_vals, 'r-', label='Теоретическая плотность')
+            if name == 'cauchy':
+                if n == 10:
+                    hist_range = (-10, 10)
+                    bins = 20
+                elif n == 100:
+                    hist_range = (-15, 15)
+                    bins = 30
+                else:
+                    hist_range = (-20, 20)
+                    bins = 100
+                ax.hist(data, bins=bins, range=hist_range, density=True, alpha=0.7,
+                        edgecolor='black', label='Гистограмма')
+                ax.set_xlim(hist_range)
+                x_vals = np.linspace(hist_range[0], hist_range[1], 200)
+                pdf_vals = dist['pdf_func'](x_vals, *dist['pdf_args'])
+                ax.plot(x_vals, pdf_vals, 'r-', label='Теоретическая плотность')
+            else:
+                if n <= 100:
+                    bins = 'sqrt'
+                else:
+                    bins = 'auto'
+                ax.hist(data, bins=bins, density=True, alpha=0.7, edgecolor='black',
+                        label='Гистограмма')
+                ax.set_xlim(dist['x_range'])
+                x_vals = np.linspace(dist['x_range'][0], dist['x_range'][1], 200)
+                pdf_vals = dist['pdf_func'](x_vals, *dist['pdf_args'])
+                ax.plot(x_vals, pdf_vals, 'r-', label='Теоретическая плотность')
             ax.set_xlabel('x')
-        
+
         ax.set_ylabel('Плотность')
         ax.set_title(f'n = {n}')
-        ax.legend(loc='upper right', fontsize=7)
+        ax.legend(loc='upper right', fontsize='small')
         ax.grid(linestyle=':', alpha=0.6)
 
     plt.tight_layout()
